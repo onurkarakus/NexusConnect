1a5cfe1dbab7a699ad66cb97b48a4ae5553fa3da
Onur KARAKUŞ <e_nigmais@hotmail.com>
2025-07-01 19:45:48 +0300

    Initial project structure

---
+++ NexusConnect.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusConnect.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ NexusConnect.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace NexusConnect.Core;
+
+public class Class1
+{
+
+}
