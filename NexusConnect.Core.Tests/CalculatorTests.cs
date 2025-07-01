2f9db593961dee068ad6ae40a1e892de3a96648f
Onur KARAKUŞ <e_nigmais@hotmail.com>
2025-07-01 19:50:31 +0300

    Add Test Example

---
+++ NexusConnect.Core.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusConnect.Core.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        // 1. Arrange (Hazırlık)
+        var calculator = new Calculator();
+        int a = 1;
+        int b = 2;
+        int expected = 3;
+
+        // 2. Act (Eylem)
+        int actual = calculator.Add(a, b);
+
+        // 3. Assert (Doğrulama)
+        Assert.Equal(expected, actual);
+    }
+}
--- NexusConnect.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace NexusConnect.Core;
-
-public class Class1
-{
-
-}
